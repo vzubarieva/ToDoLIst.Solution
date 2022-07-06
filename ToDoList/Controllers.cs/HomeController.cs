b5901ac17304967b72e5bdc9734c8328435f286e
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-07-05 20:24:49 -0700

    add loops and conditional

--- ToDoList/Controllers.cs/HomeController.cs
+++ ToDoList/Controllers.cs/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
+using System.Collections.Generic;
 
 namespace ToDoList.Controllers
 {
@@ -8,8 +9,12 @@ namespace ToDoList.Controllers
     [HttpGet("/")]
     public ActionResult Index()
     {
-      Item starterItem = new Item("Add first item to To Do List");
-      return View(starterItem);
+      List<Item> allItems = Item.GetAll();
+      return View(allItems);
+
+      // code below shows one item
+      // Item starterItem = new Item("Add first item to To Do List");
+      // return View(starterItem);
     }
 
     [HttpGet("/items/new")]
@@ -21,7 +26,11 @@ namespace ToDoList.Controllers
     public ActionResult Create(string description)
     {
       Item myItem = new Item(description);
-      return View("Index", myItem);
+      return RedirectToAction("Index");
+
+      //code below shows one item
+      // Item myItem = new Item(description);
+      // return View("Index", myItem);
     }
   }
 }
