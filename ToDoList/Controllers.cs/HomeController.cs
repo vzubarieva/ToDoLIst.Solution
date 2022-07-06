59e920996f5d08bae0e77c02d3d56664e89dea16
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-07-05 20:37:29 -0700

    separeted code so that it uses two controllers

--- ToDoList/Controllers.cs/HomeController.cs
+++ ToDoList/Controllers.cs/HomeController.cs
@@ -1,36 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
-using ToDoList.Models;
-using System.Collections.Generic;
 
 namespace ToDoList.Controllers
 {
-  public class HomeController : Controller
-  {
-    [HttpGet("/")]
-    public ActionResult Index()
+    public class HomeController : Controller
     {
-      List<Item> allItems = Item.GetAll();
-      return View(allItems);
 
-      // code below shows one item
-      // Item starterItem = new Item("Add first item to To Do List");
-      // return View(starterItem);
-    }
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+        return View();
+      }
 
-    [HttpGet("/items/new")]
-    public ActionResult CreateForm()
-    {
-      return View();
     }
-    [HttpPost("/items")]
-    public ActionResult Create(string description)
-    {
-      Item myItem = new Item(description);
-      return RedirectToAction("Index");
-
-      //code below shows one item
-      // Item myItem = new Item(description);
-      // return View("Index", myItem);
-    }
-  }
 }
