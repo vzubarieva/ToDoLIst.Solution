420f652c34d4cfd7a2c65ad3ba78b145472d9144
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-07-12 19:55:07 -0700

    add RESTful routing

--- ToDoList/Controllers.cs/ItemsController.cs
+++ ToDoList/Controllers.cs/ItemsController.cs
@@ -6,36 +6,29 @@ namespace ToDoList.Controllers
 {
     public class ItemsController : Controller
     {
-      [HttpGet("/items")]
-      public ActionResult Index()
-    {
-      List<Item> allItems = Item.GetAll();
-      return View(allItems);
-    }
+        [HttpGet("/categories/{categoryId}/items/new")]
+        public ActionResult New(int categoryId)
+        {
+            Category category = Category.Find(categoryId);
+            return View(category);
+        }
 
-    [HttpGet("/items/new")]
-    public ActionResult New()
-    {
-      return View();
-    }
+        [HttpGet("/categories/{categoryId}/items/{itemId}")]
+        public ActionResult Show(int categoryId, int itemId)
+        {
+            Item item = Item.Find(itemId);
+            Category category = Category.Find(categoryId);
+            Dictionary<string, object> model = new Dictionary<string, object>();
+            model.Add("item", item);
+            model.Add("category", category);
+            return View(model);
+        }
 
-    [HttpPost("/items")]
-    public ActionResult Create(string description)
-    {
-      Item myItem = new Item(description);
-      return RedirectToAction("Index");
-    }
-     [HttpPost("/items/delete")]
-    public ActionResult DeleteAll()
-    {
-      Item.ClearAll();
-      return View();
-    }
-    [HttpGet("/items/{id}")]
-    public ActionResult Show(int id)
-    {
-      Item foundItem = Item.Find(id);
-      return View(foundItem);
-    }
+        [HttpPost("/items/delete")]
+        public ActionResult DeleteAll()
+        {
+            Item.ClearAll();
+            return View();
+        }
     }
 }
