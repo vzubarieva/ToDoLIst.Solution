05604001181ca9a3cdc8acfdf57659e4d2f55f90
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-07-31 16:37:46 -0700

    add checkboxes to done items

---
+++ ToDoList/Migrations/20220731212740_Initial1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDoList.Migrations
+{
+    public partial class Initial1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDone",
+                table: "Items",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDone",
+                table: "Items");
+        }
+    }
+}
