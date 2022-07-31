8d0b8afd78f412517401f2a881dd36f1be537c4d
Viktoriia Z <viktoria.dubinina@gmail.com>
2022-07-31 11:42:53 -0700

    show join relationship in database

--- ToDoList/Models/Category.cs
+++ ToDoList/Models/Category.cs
@@ -6,56 +6,12 @@ namespace ToDoList.Models
     {
         public Category()
         {
-            this.Items = new HashSet<Item>();
+            // this.Items = new HashSet<Item>();
+            this.JoinEntities = new HashSet<CategoryItem>();
         }
 
         public int CategoryId { get; set; }
         public string Name { get; set; }
-        public virtual ICollection<Item> Items { get; set; }
+        public virtual ICollection<CategoryItem> JoinEntities { get; set; }
     }
 }
-
-
-
-
-
-// using System.Collections.Generic;
-
-// namespace ToDoList.Models
-// {
-//     public class Category
-//     {
-//         private static List<Category> _instances = new List<Category> { };
-//         public string Name { get; set; }
-//         public int Id { get; }
-//         public List<Item> Items { get; set; }
-
-//         public Category(string categoryName)
-//         {
-//             Name = categoryName;
-//             _instances.Add(this);
-//             Id = _instances.Count;
-//             Items = new List<Item> { };
-//         }
-
-//         public static void ClearAll()
-//         {
-//             _instances.Clear();
-//         }
-
-//         public static List<Category> GetAll()
-//         {
-//             return _instances;
-//         }
-
-//         public static Category Find(int searchId)
-//         {
-//             return _instances[searchId - 1];
-//         }
-
-//         public void AddItem(Item item)
-//         {
-//             Items.Add(item);
-//         }
-//     }
-// }
